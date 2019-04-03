a4e6bd0013006643495c482e3a69d2d990ce68de
manideep.mittal-at-580275046339 <manideepcs024@gmail.com>
2019-04-03 09:06:06 +0530

    file deleted

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -7,6 +7,7 @@ import open from 'open';
 /* eslint-disable no-console */
 
 const port = 3000;
+
 const app = express();
 const compiler = webpack(config);
 
@@ -21,7 +22,7 @@ app.get('*', function(req, res) {
   res.sendFile(path.join( __dirname, '../src/index.html'));
 });
 
-app.listen(port, function(err) {
+app.listen(process.env.PORT || 5000, function(err) {
   if (err) {
     console.log(err);
   } else {
