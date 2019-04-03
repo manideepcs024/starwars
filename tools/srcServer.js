84f79e753f4dadd447dc4414131140ebd24d32ef
manideep.mittal-at-580275046339 <manideepcs024@gmail.com>
2019-04-03 09:20:08 +0530

    file deleted

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -22,7 +22,7 @@ app.get('*', function(req, res) {
   res.sendFile(path.join( __dirname, '../src/index.html'));
 });
 
-app.listen(process.env.PORT || 5000, function(err) {
+app.listen(process.env.PORT || 3000, function(err) {
   if (err) {
     console.log(err);
   } else {
